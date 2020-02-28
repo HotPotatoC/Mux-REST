9c0a534f8feb68ecdd6a148959795d1774dda8d0
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-28 19:31:21 +0700

    Added the rest of the crud operations

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
-
-	"github.com/hotpotatoc/rest-api-mux/models"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/hotpotatoc/Mux-REST/models"
 )
 
 // Init books var as slice Book struct
-var books []Book
+var books []models.Book
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -35,46 +36,92 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	err := json.NewEncoder(w).Encode(&Book{})
+	err := json.NewEncoder(w).Encode(&models.Book{})
 	if err != nil {
 		return
 	}
 }
 
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book models.Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
 
+	book.ID = strconv.Itoa(rand.Intn(10000000)) // mock id
+	books = append(books, book)
+
+	err := json.NewEncoder(w).Encode(book)
+	if err != nil {
+		return
+	}
 }
 
-func editBook(w http.ResponseWriter, r *http.Request) {
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
+	for index, book := range books {
+		if book.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book models.Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+
+			book.ID = params["id"]
+			books = append(books, book)
+
+			err := json.NewEncoder(w).Encode(book)
+			if err != nil {
+				return
+			}
+			return
+		}
+	}
+
+	err := json.NewEncoder(w).Encode(books)
+	if err != nil {
+		return
+	}
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
+	for index, book := range books {
+		if book.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+
+	err := json.NewEncoder(w).Encode(books)
+	if err != nil {
+		return
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
 
 	// Mock data
-	books = append(books, Book{
+	books = append(books, models.Book{
 		ID:     "1",
 		Isbn:   "2462462351",
 		Title:  "A Tale of Two Cities",
-		Author: &Author{Firstname: "John", Lastname: "Doe"},
+		Author: &models.Author{Firstname: "John", Lastname: "Doe"},
 	})
-	books = append(books, Book{
+	books = append(books, models.Book{
 		ID:     "2",
 		Isbn:   "9456846859",
 		Title:  "The Busy Road",
-		Author: &Author{Firstname: "Richard", Lastname: "Smith"},
+		Author: &models.Author{Firstname: "Richard", Lastname: "Smith"},
 	})
 
 	// Register endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", editBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 	fmt.Printf("Server started")
